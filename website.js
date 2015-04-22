3865a3393b1219e61ee60b98566f871651b12ba5
Gaye Bulut <gayegul@gmail.com>
2015-04-21 20:37:59 -0700

    change main page design

--- website.js
+++ website.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var generateBox = function() {
-  for(var i = 0; i < 60; i++){
+  for(var i = 0; i < 55; i++) {
     var newBox = document.createElement("div");
     newBox.className = "smallBox";
     document.getElementById("bigBox").appendChild(newBox);
-    newBox.onmouseover = function(){
+    newBox.onmouseover = function() {
       var newColor = "rgb(" + Math.floor(Math.random()*50) + "," + Math.floor(100 + Math.random()*256) + "," + Math.floor(100 + Math.random()*256) + ")";
       this.style.backgroundColor = newColor;
     }
